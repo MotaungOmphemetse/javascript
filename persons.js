b68b9007747de3a9c94028d1e1a9764e0d7b1743
MotaungOmphemetse <motau.omphemetse18@gmail.com>
2023-11-06 15:20:19 +0200

    a new object of studen, person added

--- persons.js
+++ persons.js
@@ -10,3 +10,19 @@ let ntombi = new Person("Ntombi",32);
 
 console.log(omphemetse);
 console.log(ntombi);
+
+let johny = {
+    name: 'Johny',
+    age: 24
+};
+
+ function Students(name, age, course) {
+    this.name = name;
+    this.age = age;
+    this.course = course;
+ }
+
+let ntombii = new Students("Thoko", 12, "Fullstack");
+// let john = new Students();
+console.log(ntombii);
+console.log(johny);
